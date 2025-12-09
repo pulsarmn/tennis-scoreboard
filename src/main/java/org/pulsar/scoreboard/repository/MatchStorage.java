1dce46b5137da50053da47179c9911175b3e520c
pulsar <pulsarmn@yandex.ru>
2025-12-09 20:00:42 +0300

    feat: implemet the set() method in the MatchStorage class

--- src/main/java/org/pulsar/scoreboard/repository/MatchStorage.java
+++ src/main/java/org/pulsar/scoreboard/repository/MatchStorage.java
@@ -1,12 +1,27 @@
 package org.pulsar.scoreboard.repository;
 
+import org.pulsar.scoreboard.model.MatchScore;
 import redis.clients.jedis.Jedis;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.UUID;
 
 public class MatchStorage {
 
     private final Jedis jedis;
+    private final ObjectMapper objectMapper;
 
-    public MatchStorage(Jedis jedis) {
+    public MatchStorage(Jedis jedis, ObjectMapper objectMapper) {
         this.jedis = jedis;
+        this.objectMapper = objectMapper;
+    }
+
+    public void set(UUID matchId, MatchScore matchScore) {
+        if (matchId == null || matchScore == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String jsonMatch = objectMapper.writeValueAsString(matchScore);
+        jedis.set(matchId.toString(), jsonMatch);
     }
 }
