0b910a19a832a1c9a8049b82ce9f1373788d4e9d
pulsarmn <152626412+pulsarmn@users.noreply.github.com>
2025-12-10 07:54:56 +0300

    Connection to redis
    * chore: add Postgresql JDBC dependency
    
    * chore: add Jedis dependency
    
    * feat: create a PlayerScore class
    
    * feat: create a MatchScore class
    
    * feat: create a MatchStorage class
    
    * chore: add Jackson-databind dependency
    
    * feat: implemet the set() method in the MatchStorage class
    
    * feat: implement the get() method in the MatchStorage class

---
+++ src/main/java/org/pulsar/scoreboard/repository/MatchStorage.java
@@ -0,0 +1,41 @@
+package org.pulsar.scoreboard.repository;
+
+import org.pulsar.scoreboard.model.MatchScore;
+import redis.clients.jedis.Jedis;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.UUID;
+
+public class MatchStorage {
+
+    private final Jedis jedis;
+    private final ObjectMapper objectMapper;
+
+    public MatchStorage(Jedis jedis, ObjectMapper objectMapper) {
+        this.jedis = jedis;
+        this.objectMapper = objectMapper;
+    }
+
+    public void set(UUID matchId, MatchScore matchScore) {
+        if (matchId == null || matchScore == null) {
+            throw new IllegalArgumentException();
+        }
+
+        String jsonMatch = objectMapper.writeValueAsString(matchScore);
+        jedis.set(matchId.toString(), jsonMatch);
+    }
+
+    public MatchScore get(UUID matchId) {
+        if (matchId == null) {
+            return null;
+        }
+
+        String jsonMatch = jedis.get(matchId.toString());
+        try {
+            return objectMapper.readValue(jsonMatch, MatchScore.class);
+        } catch (Exception e) {
+            // TODO: handle exception normally
+            return null;
+        }
+    }
+}
